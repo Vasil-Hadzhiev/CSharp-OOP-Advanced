f3c9ef8bc1146099db47b6461fb40eb9c5d1defd
Vasil <vsl.h.94@gmail.com>
2017-07-22 12:36:53 +0200

    Completed Interfaces and Abstraction LAB

---
+++ Interfaces and Abstraction/InterfacesAndAbstractionLAB/01.Shapes/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Circle : IDrawable
+{
+    private int radius;
+
+    public Circle(int radius)
+    {
+        this.Radius = radius;
+    }
+
+    public int Radius
+    {
+        get { return this.radius; }
+        private set { this.radius = value; }
+    }
+
+    public void Draw()
+    {
+        double rIn = this.radius - 0.4;
+        double rOut = this.radius + 0.4;
+
+        for (double i = this.radius; i >= -this.radius; i--)
+        {
+            for (double j = -this.radius; j < rOut; j += 0.5)
+            {
+                double value = i * i + j * j;
+
+                if (value >= rIn * rIn && value <= rOut * rOut)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
