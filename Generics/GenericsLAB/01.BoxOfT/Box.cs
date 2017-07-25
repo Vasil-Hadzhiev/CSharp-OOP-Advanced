ab7349ca93ee3af339a5050d16f1520b6dcf8582
Vasil <vsl.h.94@gmail.com>
2017-07-25 19:12:40 +0200

    Completed Generics Lab

---
+++ Generics/GenericsLAB/01.BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    private IList<T> boxList;
+
+    public Box()
+    {
+        this.boxList = new List<T>();   
+    }
+
+    public int Count => this.boxList.Count;
+
+    public void Add(T element)
+    {
+        this.boxList.Add(element);
+    }
+
+    public T Remove()
+    {
+        var lastItem = this.boxList.LastOrDefault();
+        this.boxList.RemoveAt(this.boxList.Count - 1);
+
+        return lastItem;
+    }
+}
+
