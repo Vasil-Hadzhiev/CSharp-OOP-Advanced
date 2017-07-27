0bb563ce7c3a6427c65f4683b3581c3ab30fbf6d
Vasil <vsl.h.94@gmail.com>
2017-07-27 10:35:39 +0200

    Completed exercises 11 - 12

---
+++ Generics/GenericsExercises/12.Threeuple/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var name = string.Empty;
+
+        var firstInput = Console.ReadLine().Split();
+        name = firstInput[0] + " " + firstInput[1];
+        var address = firstInput[2];
+        var town = firstInput[3];
+        var firstGenericTuple = new GenericTuple<string, string, string>(name, address, town);
+        Console.WriteLine(firstGenericTuple.ToString());
+
+        var secondInput = Console.ReadLine().Split();
+        name = secondInput[0];
+        var litersOfBeer = int.Parse(secondInput[1]);
+        var isDrunk = false;
+        if (secondInput[2] == "drunk")
+        {
+            isDrunk = true;
+        }
+        else
+        {
+            isDrunk = false;
+        }
+        var secondGenericTuple = new GenericTuple<string, int, bool>(name, litersOfBeer, isDrunk);
+        Console.WriteLine(secondGenericTuple.ToString());
+
+        var thirdInput = Console.ReadLine().Split();
+        name = thirdInput[0];
+        var accountBalance = double.Parse(thirdInput[1]);
+        var bankName = thirdInput[2];
+        var thirdGenericTuple = new GenericTuple<string, double, string>(name, accountBalance, bankName);
+        Console.WriteLine(thirdGenericTuple.ToString());
+    }
+}
+
