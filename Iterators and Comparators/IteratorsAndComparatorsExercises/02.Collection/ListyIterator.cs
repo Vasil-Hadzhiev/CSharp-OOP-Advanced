9ddc951630bc303a9076c2ad24458a444b11d865
Vasil <vsl.h.94@gmail.com>
2017-07-28 16:29:15 +0200

    Completed exercises 1 - 5

---
+++ Iterators and Comparators/IteratorsAndComparatorsExercises/02.Collection/ListyIterator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private IList<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(IList<T> elements)
+        {
+            this.currentIndex = 0;
+            this.elements = elements;
+        }
+
+        public bool Move()
+        {
+            if (this.currentIndex < this.elements.Count - 1)
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex == this.elements.Count - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Any())
+            {
+                Console.WriteLine(this.elements[this.currentIndex]);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (this.elements.Any())
+            {
+                for (int i = 0; i < this.elements.Count; i++)
+                {
+                    yield return this.elements[i];
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/IteratorsAndComparatorsExercises/05.ComparingObjects/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person otherPerson)
+        {
+            if (this.name != otherPerson.name)
+            {
+                return this.name.CompareTo(otherPerson.name);
+            }
+
+            if (this.age != otherPerson.age)
+            {
+                return this.age.CompareTo(otherPerson.age);
+            }
+
+            return this.town.CompareTo(otherPerson.town);
+        }
+    }
+}
