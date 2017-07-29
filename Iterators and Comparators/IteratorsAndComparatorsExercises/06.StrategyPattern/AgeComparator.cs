468c65ccb41f0d01d9e3e621fea3fef5facb1ac9
Vasil <vsl.h.94@gmail.com>
2017-07-29 11:36:12 +0200

    Completed exercise 6

---
+++ Iterators and Comparators/IteratorsAndComparatorsExercises/06.StrategyPattern/AgeComparator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace _06.StrategyPattern
+{
+    public class AgeComparator : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person secondPerson)
+        {
+            var result = firstPerson.Age.
+                CompareTo(secondPerson.Age);
+
+            if (firstPerson.Age != secondPerson.Age)
+            {
+                return result;
+            }
+
+            return 0;
+        }
+    }
+}
