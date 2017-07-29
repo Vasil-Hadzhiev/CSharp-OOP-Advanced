104625844c5d1e0165388c000505322abc744ce3
Vasil <vsl.h.94@gmail.com>
2017-07-29 23:07:21 +0200

    Completed exercise 7

---
+++ Iterators and Comparators/IteratorsAndComparatorsExercises/07.EqualityLogic/CustomComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace _07.EqualityLogic
+{
+    public class CustomComparer : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person secondPerson)
+        {
+            var result = firstPerson.Name.
+                CompareTo(secondPerson.Name);
+
+            if (result == 0)
+            {
+                result = firstPerson.Age.
+                    CompareTo(secondPerson.Age);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Iterators and Comparators/IteratorsAndComparatorsExercises/07.EqualityLogic/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var sorted = new SortedSet<Person>(new CustomComparer());
+            var hash = new HashSet<Person>(new CustomEqualityComparer());
+
+            for (int i = 0; i < n; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+
+                var person = new Person(name, age);
+                sorted.Add(person);
+                hash.Add(person);
+            }
+
+            Console.WriteLine(sorted.Count());
+            Console.WriteLine(hash.Count());
+        }
+    }
+}
