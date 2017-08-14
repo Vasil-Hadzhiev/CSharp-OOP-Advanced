765ef19fefff7d20ceb90c5945dba1bd49c6ed81
Vasil <vsl.h.94@gmail.com>
2017-08-14 10:21:26 +0200

    Updated exercise 3

--- Reflection/ReflectionExercises/03.BarracksFactory/Core/Factories/UnitFactory.cs
+++ Reflection/ReflectionExercises/03.BarracksFactory/Core/Factories/UnitFactory.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using _03.BarracksFactory.Contracts;
 using _03.BarracksFactory.Models.Units;
+using System.Reflection;
+using System.Linq;
 
 namespace _03.BarracksFactory.Core.Factories
 { 
     public class UnitFactory : IUnitFactory
     {
-        private const string UnitNamespace = "_03.BarracksFactory.Models.Units.";
-
         public IUnit CreateUnit(string unitType)
         {
-            var typeUnit = Type.GetType(unitType);
-            var unitInstance = (IUnit)Activator.CreateInstance(typeUnit);
-            return unitInstance;
+            var typeUnit = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == unitType);
+            return (IUnit)Activator.CreateInstance(typeUnit);
         }
     }
 }
