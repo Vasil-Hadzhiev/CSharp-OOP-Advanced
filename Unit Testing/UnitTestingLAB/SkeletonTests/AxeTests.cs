6acbfec85ff9704c3854ad8a49d3013dff666788
Vasil <vsl.h.94@gmail.com>
2017-08-15 10:21:39 +0200

    Completed Unit Testing Lab

---
+++ Unit Testing/UnitTestingLAB/SkeletonTests/AxeTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SkeletonTests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private const int AxeAttack = 1;
+        private const int AxeDurability = 1;
+        private const int DummyHealth = 10;
+        private const int DummyExperience = 10;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+        }
+
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            //Arange - done in TestInit method.
+
+            //Act
+            this.axe.Attack(this.dummy);
+
+            //Assert
+            Assert.AreEqual(9, this.axe.DurabilityPoints, "Axe Durability doesn't change after atack");
+        }
+
+        [Test]
+        public void BrokenAxeAttack()
+        {
+            //Arange - done in TestInit method.
+
+            //Act
+            this.axe.Attack(this.dummy);
+
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => this.axe.Attack(this.dummy));
+            Assert.AreEqual("Axe is broken.", ex.Message);
+        }
+    }
+}
---
+++ Unit Testing/UnitTestingLAB/SkeletonTests/DummyTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SkeletonTests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private const int DummyHealth = 10;
+        private const int DummyXp = 10;
+
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            this.dummy = new Dummy(DummyHealth, DummyXp);
+        }
+
+        [Test]
+        public void DummyLosesHealthWhenAttacked()
+        {
+            //Arrange - done in TestInit method.
+
+            //Act
+            this.dummy.TakeAttack(5);
+
+            //Assert
+            Assert.IsTrue(this.dummy.Health < DummyHealth, "Dummy doesn't lose health after attack");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionWhenAttacked()
+        {
+            //Arrange - done in TestInit method.
+
+            //Act
+            this.dummy.TakeAttack(10);
+
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => this.dummy.TakeAttack(10));
+            Assert.That(ex.Message, Is.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            //Arrange - done in TestInit method.
+
+            //Act
+            while (!this.dummy.IsDead())
+            {
+                this.dummy.TakeAttack(10);
+            }
+
+            var experienceGot = this.dummy.GiveExperience();
+
+            //Assert
+            Assert.IsTrue(DummyXp == experienceGot);
+        }
+
+        [Test]
+        public void DeadDummyCannotGiveXp()
+        {
+            //Arrange - done in TestInit method.
+
+            //Act
+
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => this.dummy.GiveExperience());
+            Assert.That(ex.Message, Is.EqualTo("Target is not dead."));
+        }
+    }
+}
