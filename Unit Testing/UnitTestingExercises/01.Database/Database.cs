fb90b9eec69c2a5074b1a85eb419afac23e27b64
Vasil <vsl.h.94@gmail.com>
2017-08-17 11:14:30 +0200

    Init upload

---
+++ Unit Testing/UnitTestingExercises/01.Database/Database.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Database
+{
+    public class Database
+    {
+        private const int Capacity = 16;
+
+        private int[] elements;
+        private int count;
+
+        public Database()
+        {
+            this.elements = new int[Capacity];
+        }
+
+        public Database(params int[] numbers)
+            :this()
+        {
+            if (numbers != null)
+            {
+                foreach (var item in numbers)
+                {
+                    this.Add(item);
+                }
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (this.count == this.elements.Length)
+            {
+                throw new InvalidOperationException("Can't add more elements to array");
+            }
+
+            this.elements[this.count] = element;
+            this.count++;
+        }
+
+        public void Remove()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+
+            this.count--;
+        }
+
+        public int[] Fetch()
+        {
+            return this.elements.Take(this.count).ToArray();
+        }
+    }
+}
