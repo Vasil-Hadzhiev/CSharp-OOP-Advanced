6fd7a0b9da1089279df7a108ff1d70267bfba752
Vasil <vsl.h.94@gmail.com>
2017-08-18 01:13:26 +0200

    Init upload

---
+++ SOLID/SOLID-LAB/01.Stream Progress/IStreamable.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.Stream_Progress
+{
+    public interface IStreamable
+    {
+        int BytesSent { get; }
+
+        int Length { get; }
+    }
+}
---
+++ SOLID/SOLID-LAB/03.Detail Printer/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace _03.Detail_Printer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
